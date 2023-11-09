77a45c49ac011131ec30fce69348d19a71a6f799
Danila Solovenko <523944@student.fontys.nl>
2023-11-09 23:55:15 +0100

    added departments functionality

--- GroupProject/DesktopAppMediaBazaar/Forms/Departments.cs
+++ GroupProject/DesktopAppMediaBazaar/Forms/Departments.cs
@@ -1,4 +1,6 @@
-﻿using DataItems.LogicItems;
+﻿using BussinessLayer.Controllers;
+using DataAccessLayer.DAL;
+using DataItems.LogicItems;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,11 +15,44 @@ namespace DesktopAppMediaBazaar.Forms
 {
     public partial class Departments : Form
     {
+        private DepartmentController DepartmentController;
         private Employee _loggedInEmployee;
+
         public Departments(Employee loggedInEmployee)
         {
             InitializeComponent();
             _loggedInEmployee = loggedInEmployee;
+            DepartmentController = new(new DALDepartmentController());
+            showDepartments();
+        }
+
+        private void showDepartments()
+        {
+            foreach (Department department in DepartmentController.GetAll())
+            {
+                lbDepartments.Items.Add(department.Name);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (tbxName.Texts != string.Empty)
+            {
+                Department department = new Department(tbxName.Texts);
+                DepartmentController.Create(department);
+                lbDepartments.Items.Add(department.Name);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lbDepartments.SelectedIndex != -1)
+            {
+                Department dep = DepartmentController.Get(lbDepartments.SelectedItem.ToString());
+                DepartmentController.Remove(dep);
+                lbDepartments.Items.Clear();
+                showDepartments();
+            }
         }
     }
 }
